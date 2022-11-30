3f63b3999061333add0a0d6ae4cb579d5dced0e7
Renier Vargas <rvargas@fenacrep.org>
2022-11-29 20:16:33 -0500

    update instalacion de Tailwind

--- src/js/TodoCounter.js
+++ src/js/TodoCounter.js
@@ -3,7 +3,7 @@ import React from 'react';
 function TodoCounter() {
     return (
         <React.Fragment>
-            <div className='mt-2'>
+            <div className='bg-slate-500'>
                 <h2> Has completado 2 de 3 Tareas</h2>
             </div>
         </React.Fragment>
