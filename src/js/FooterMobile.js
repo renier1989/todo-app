4fb9459ae79c014403d867d7b406f1a9af4796f2
renier1989 <renier1989@gmail.com>
2022-12-21 11:57:24 -0500

    fix enlaces externos en mobile

--- src/js/FooterMobile.js
+++ src/js/FooterMobile.js
@@ -6,41 +6,71 @@ function FooterMobile(props) {
     }
 return (
     <div className="bg-primary w-full h-16 fixed left-0 bottom-0 shadow-md flex space-x-8 items-center justify-center lg:hidden dark:bg-gray-600">
-    <a href="#" className="">
-        <svg
-        onClick={buttonClick}
-        xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor"
-        className="bi bi-plus-circle-fill w-11 h-11 cursor-pointer"
-        viewBox="0 0 16 16"
+        {/* Icono de GitHub */}
+        <a href="https://github.com/renier1989/todo-app" target="_blank">
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-moon-stars-fill w-6 h-6 cursor-pointer"
+            viewBox="0 0 16 16"
+            >
+            <path
+                className="fill-current text-secondary hover:text-secondary-dark text-center "
+                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+            />
+            </svg>
+        </a>
+        <a href="#" className="">
+            <svg
+            onClick={buttonClick}
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            className="bi bi-plus-circle-fill w-11 h-11 cursor-pointer"
+            viewBox="0 0 16 16"
+            >
+            <path
+                className="fill-current text-secondary hover:text-secondary-dark text-center"
+                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"
+            />
+            </svg>
+        </a>
+        <a href="#" id="toggle-sm" 
+        onClick={props.changeDarkMode}
         >
-        <path
-            className="fill-current text-secondary hover:text-secondary-dark text-center"
-            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"
-        />
-        </svg>
-    </a>
-    <a href="#" id="toggle-sm" 
-    // onClick={props.changeDarkMode}
-    onDoubleClick={props.changeDarkMode}
-    >
-        <svg
-        
-        xmlns="http://www.w3.org/2000/svg"
-        fill="currentColor"
-        className="bi bi-moon-stars-fill w-11 h-11 cursor-pointer"
-        viewBox="0 0 16 16"
-        >
-        <path
-            className="fill-current text-secondary"
-            d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"
-        />
-        <path
-            className="fill-current text-secondary"
-            d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"
-        />
-        </svg>
-    </a>
+            <svg
+            
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            className="bi bi-moon-stars-fill w-11 h-11 cursor-pointer"
+            viewBox="0 0 16 16"
+            >
+            <path
+                className="fill-current text-secondary"
+                d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z"
+            />
+            <path
+                className="fill-current text-secondary"
+                d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z"
+            />
+            </svg>
+        </a>
+        {/* Icono de Linkedin */}
+        <a href="https://www.linkedin.com/in/renier-vargas-0a971b125/" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-moon-stars-fill w-6 h-6 cursor-pointer bi bi-linkedin"
+                viewBox="0 0 16 16"
+              >
+                <path 
+                className="fill-current text-secondary hover:text-secondary-dark text-center "
+                d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
+              </svg>
+            </a>
     </div>
 );
 }
